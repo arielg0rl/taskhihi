23aa2646679b4628bc8bfd53b17cd064dbd4ff9c
Irka Shemchuk <89932902+arielg0rl@users.noreply.github.com>
2022-01-11 13:21:53 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  return (
+    <div className="App">Hello goy
+    </div>
+  );
+}
+
+export default App;
